6fdbbb043c24ea82fe3500d1356ffe1b4128a948
ahmadhipnie <112534396+ahmadhipnie@users.noreply.github.com>
2025-08-04 20:48:07 +0700

    pembaruan email

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,7 +10,7 @@
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Karyawan Pengaduan</p>
+                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Transaksi</p>
                                     <h5 class="font-weight-bolder">
                                         0
                                     </h5>
@@ -31,9 +31,7 @@
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Email Belum
-                                        Dibaca
-                                    </p>
+                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Pendapatan</p>
                                     <h5 class="font-weight-bolder text-danger">
                                         0
                                     </h5>
@@ -54,7 +52,7 @@
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Email Sudah Dibaca</p>
+                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Transaksi Hari ini</p>
                                     <h5 class="font-weight-bolder text-danger">
                                         0
                                     </h5>
@@ -69,27 +67,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
-                    <div class="card-body p-3">
-                        <div class="row">
-                            <div class="col-8">
-                                <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Email Sudah Dibalas</p>
-                                    <h5 class="font-weight-bolder text-success">
-                                        0
-                                    </h5>
-                                </div>
-                            </div>
-                            <div class="col-4 text-end">
-                                <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
-                                    <i class="ni ni-email-83 text-lg opacity-10" aria-hidden="true"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </div>
 
     </div>
---
+++ resources/views/admin/karyawan_admin/karyawan_admin.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.sidebar')
+@section('title', 'Karyawan')
+@section('content')
+
+<div class="container-fluid py-4">
+
+    
+
+</div>
+@endsection
